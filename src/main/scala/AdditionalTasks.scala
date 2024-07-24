49e520429aee971e5df152f110ea59e865c38b63
e.shah <eshashah2003@gmail.com>
2024-07-24 13:02:57 +0530

    additional tasks changes

--- src/main/scala/AdditionalTasks.scala
+++ src/main/scala/AdditionalTasks.scala
@@ -1,30 +1,37 @@
 object AdditionalTasks extends App {
 
   //Q1
-  def getSize[T](l: List[T], accum: Int): Int = {
-    if (l.isEmpty) accum
-    else {
-      getSize(l.tail, 1 + accum)
-    }
+  def getSize[T](l: List[T]): Int = {
+    l.foldLeft(0)((counter, _) => counter + 1)
   }
+
+  //  def middle[T](list: List[T]): Option[T] = {
+  //    def middleAux[T](list: List[T], middle: Int, index: Int): Option[T] = {
+  //      if (list.isEmpty) None
+  //      else if (getSize(list) == 1) Some(list.head)
+  //      else {
+  //        if (index == middle) Some(list.head)
+  //        else middleAux(list.tail, middle, index + 1)
+  //      }
+  //    }
+  //
+  //    val size = getSize(list) / 2
+  //    val middleEle = if (size % 2 == 0) size else size - 1
+  //    middleAux(list, middleEle, 0)
+  //  }
 
   def middle[T](list: List[T]): Option[T] = {
-    def middleAux[T](list: List[T], middle: Int, index: Int): Option[T] = {
-      if (list.isEmpty) None
-      else if (getSize(list, 0) == 1) Some(list.head)
-      else {
-        if (index == middle) Some(list.head)
-        else middleAux(list.tail, middle, index + 1)
-      }
+    def middleRec(remaining: List[T]): Option[T] = remaining match {
+      case (m :: Nil) => Some(m) //one element
+      case (m :: _ :: Nil) => Some(m) //two elements
+      case (_ :: _) => middleRec(remaining.tail.init)
+      case _ => None
     }
 
-    val size = getSize(list, 0) / 2
-    val middleEle = if (size % 2 == 0) size else size - 1
-    middleAux(list, middleEle, 0)
+    middleRec(list)
   }
 
-
-  val list1 = middle(List(1, 4, 3, 2, 5)) //Some(3)
+  val list1 = middle(List(1, 2, 3, 4, 5)) //Some(3)
   val list2 = middle(List(1, 2, 3, 4, 5, 6)) //Some(3)
   val list3 = middle(List(1)) // Some(1)
   val list4 = middle(List()) // None
@@ -40,44 +47,29 @@ object AdditionalTasks extends App {
   //Q2
 
   var charArray: Array[Char] = Array()
+  var findLetterMap: Map[Int, Char] = Map()
+  var findOccurMap: Map[Char, Int] = Map()
 
   def Initialize(array: Array[Char]): Unit = {
     charArray = array
+    findLetterMap = charArray.zipWithIndex.map(_.swap).toList.toMap
+    // println(findLetterMap)
+    findOccurMap = charArray.groupBy(x => x).mapValues(_.size).toMap
+    println(findOccurMap)
   }
 
   def FindOccurence(input: Char): Int = {
-    def FindOccurenceAux(list: Array[Char], input: Char, accum: Int): Int = {
-      if (list.isEmpty) accum
-      else {
-        if (list.head == input) FindOccurenceAux(list.tail, input, 1 + accum)
-        else FindOccurenceAux(list.tail, input, accum)
-      }
-    }
-
-    FindOccurenceAux(charArray, input, 0)
+    findOccurMap.getOrElse(input, 0)
   }
 
   def FindLetter(input: Int): Char = {
-    def FindLetterAux(list: Array[Char], input: Int, counter: Int): Char = {
-      if (list.isEmpty) {
-        throw new NoSuchElementException("List is empty.")
-      }
-      if (input == counter) list.head
-      else {
-        FindLetterAux(list.tail, input, 1 + counter)
-      }
-    }
-
-    FindLetterAux(charArray, input, 0)
+    findLetterMap.getOrElse(input, 0)
   }
 
   val myList = Array('a', 'b', 'a', 'c', 'c', 'a')
-  charArray = myList
+  Initialize(myList)
   val x = FindOccurence('a')
   println("Occurence: " + x)
-  val y = FindLetter(2)
+  val y = FindLetter(4)
   println("Character: " + y)
 }
-
-
-
